63817997021d277a09b88a2df70b5cd3674bde4e
MarkHamiltonII <markhamiltonii@gmail.com>
2022-07-26 17:01:05 -0500

    Added security basics

---
+++ server/survivor-api/src/main/java/survivor/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package survivor.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
+        http.authorizeRequests()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/create_account").permitAll()
+                .antMatchers("/api/refresh_token").authenticated()
+                .antMatchers(HttpMethod.GET,"/api/users").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/api/users/*").hasAnyRole("USER")
+                .antMatchers(HttpMethod.PUT,"/api/users/*").hasAnyRole("USER")
+                .antMatchers(HttpMethod.DELETE,"/api/users/*").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/castaway/all").permitAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(),converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+
+}
