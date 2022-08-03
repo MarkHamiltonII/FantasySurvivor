443602d2d64b0a2843f013a0dd1bc9d7c724d3b4
MarkHamiltonII <markhamiltonii@gmail.com>
2022-08-03 12:33:18 -0500

    Added controller for create/update/delete league

--- server/survivor-api/src/main/java/survivor/controllers/LeagueController.java
+++ server/survivor-api/src/main/java/survivor/controllers/LeagueController.java
@@ -3,10 +3,7 @@ package survivor.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import survivor.domain.LeagueService;
 import survivor.domain.Result;
 import survivor.models.AppUser;
@@ -44,6 +41,33 @@ public class LeagueController {
         Result<?> result = service.findLeaguesByAppUserId(appUser.getAppUserId());
         if (result.isSuccess()){
             return new ResponseEntity<>(result.getPayload(), HttpStatus.OK);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PostMapping("/new_league")
+    public ResponseEntity<?> createLeague(@RequestBody League league){
+        Result<?> result = service.createLeague(league);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/change_league")
+    public ResponseEntity<?> updateLeague(@RequestBody League league){
+        Result<?> result = service.updateLeague(league);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/league{id}")
+    public ResponseEntity<?> deteleLeague(@PathVariable int id){
+        Result<?> result = service.deleteLeagueById(id);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return ErrorResponse.build(result);
     }
