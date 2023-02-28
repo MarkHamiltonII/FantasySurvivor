d0b2cb3da994c06cb06a36ce1835a1075716c0df
MarkHamiltonII <markhamiltonii@gmail.com>
2023-02-28 02:53:50 -0600

    Added ability to view other player's lists'

--- fantasy-survivor-client/src/components/LeagueUser.js
+++ fantasy-survivor-client/src/components/LeagueUser.js
@@ -1,12 +1,67 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import AuthContext from "../AuthContext";
 import Header from "./Header";
+import CastawayList from "./CastawayList";
 
 function LeagueUser() {
 
-    const {leagueId, userId} = useParams();
+    const auth = useContext(AuthContext);
+    const { leagueId, userId } = useParams();
+    const [rating, setRating] = useState({});
+    const [fetchAttempt, setFetchAttmept] = useState(false)
+    const [user, setUser] = useState({})
+    const [userFetchAttempt, setUserFetchAttempt] = useState(false)
+
+    useEffect(() => {
+
+        const init = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${auth.user.token}`
+            }
+        };
+
+        fetch(`${process.env.REACT_APP_API_URL}/api/rating/league${leagueId}/user${userId}`, init)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    return Promise.reject(`Unexpected status code: ${response.status}`);
+                }
+            })
+            .then(d => {
+                setRating(d);
+                setFetchAttmept(true);
+            })
+            .catch(console.log)
+
+        fetch(`${process.env.REACT_APP_API_URL}/api/users/${userId}`, init)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    setUserFetchAttempt(true);
+                    return Promise.reject(`Unexpected status code: ${response.status}`);
+                }
+            })
+            .then(d => {
+                setUser(d);
+                setUserFetchAttempt(true);
+            })
+            .catch(console.log)
+
+    }, [auth, userId, leagueId])
 
     return (
-        <Header heading={`League ${leagueId}: User ${userId}`} />
+        <>
+            {userFetchAttempt && <Header heading={`${user.username}'s Ranking`} />}
+            {rating && fetchAttempt &&
+                <div className="justify-center">
+                    <CastawayList castaways={rating.castaways} />
+                </div>}
+        </>
     )
 }
 
